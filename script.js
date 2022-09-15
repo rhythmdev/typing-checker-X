62398f0ffb71ddbbd194d950581b2ebca0445f28
rhythmdev <rhythmbiswas@gmail.com>
2022-09-15 17:30:50 +0600

    new math method added

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = parseInt((finishTime - startTime) / 1000);
+  const timeTaken = Math.trunc((finishTime - startTime) / 1000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -139,7 +139,7 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = parseInt((currentTime - startTime) / 1000);
+  const timeSpent = Math.trunc((currentTime - startTime) / 1000);
 
   document.getElementById("show-time").innerHTML = `${
     startTime ? timeSpent : 0
